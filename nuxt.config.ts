c9776d552c4e36fe1dbbea400f536386bec955c5
Beardography <ryan.l.huber@gmail.com>
2022-08-08 20:59:37 -0500

    setting up documents and styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    css: ["~/assets/styles/styles.css"],
     modules: ["@nuxt/content"],
     content: {},
 });
