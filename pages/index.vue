0df95084876b584b1a73fadfc4bbf955681df5f0
Beardography <ryan.l.huber@gmail.com>
2022-08-08 20:44:08 -0500

    set up a way to automatically add new recipes to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
     <main>
-        <ContentDoc />
+        <h1>All Recipes</h1>
+        <ContentList path="/recipes" v-slot="{ list }">
+            <div v-for="recipe in list" :key="recipe._path">
+                <NuxtLink :to="recipe._path">
+                    {{ recipe.title }}
+                </NuxtLink>
+            </div>
+        </ContentList>
     </main>
 </template>
