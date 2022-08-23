749f145b8da519f16243e6a953437711a0b46bf6
Beardography <ryan.l.huber@gmail.com>
2022-08-22 19:37:06 -0500

    testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 import { defineNuxtConfig } from "nuxt";
+import { Static } from "vue";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: ["~/assets/styles/styles.css"],
     modules: ["@nuxt/content"],
-    ssr: true
+    ssr: false,
+    target: 'static'
 });
