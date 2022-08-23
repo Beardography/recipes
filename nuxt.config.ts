88754f22919ca00a0ef9d4e87a7622046da474b9
Beardography <ryan.l.huber@gmail.com>
2022-08-22 19:58:59 -0500

    reverted back

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,6 @@ import { Static } from "vue";
 export default defineNuxtConfig({
     css: ["~/assets/styles/styles.css"],
     modules: ["@nuxt/content"],
-    ssr: true,
+    ssr: false,
     target: 'static'
 });
