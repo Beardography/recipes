a78b300e71fbbd2eff6dbfeb9e1b4a2caaa7e9f5
Beardography <ryan.l.huber@gmail.com>
2022-08-22 20:59:40 -0500

    setting stuff up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 import { defineNuxtConfig } from "nuxt";
-import { Static } from "vue";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     css: ["~/assets/styles/styles.css"],
-    modules: ["@nuxt/content"],
+    modules: ["@nuxt/content", "@nuxtjs/partytown"],
     ssr: false,
-    target: 'static'
+    target: "static",
+    nitro: {
+        preset: 'service-worker'
+    }
 });
